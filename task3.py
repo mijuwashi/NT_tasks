231fc16521acea62086fb12f56821aebe2a35ee4
mijuwashi <71728133+mijuwashi@users.noreply.github.com>
2023-04-10 14:53:02 +0300

    Add files via upload

---
+++ task3.py
@@ -0,0 +1,16 @@
+import json
+
+with open('C:/Users/Lenovo/Downloads/tests.json', 'r') as f1, open('C:/Users/Lenovo/Downloads/values.json', 'r') as f2:
+    tests_data = json.load(f1)
+    values_data = json.load(f2)
+
+for test in tests_data:
+    for value in values_data:
+        if value['id'] == test['id']:
+            test['value'] = value['value']
+    for val in test['values']:
+        if val['id'] == value['id']:
+            val['value'] = value['value']
+
+with open('report.json', 'w') as f3:
+    json.dump(tests_data, f3)
